19bd46dccf07d91eae8f18c1385caf257fc9d534
regyanax <miftahregyana@gmail.com>
2025-03-09 20:38:38 +0700

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,26 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useWebSocketAdapter(new IoAdapter(app));
+
+  const config = new DocumentBuilder()
+    .setTitle('Resistance API')
+    .setDescription('The Resistance Game API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
