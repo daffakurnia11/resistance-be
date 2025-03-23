8fd1a573935f9b1588ec6d06acc8fda2842cf448
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2025-03-23 20:40:59 +0700

    feat: :sparkles: Include players data in lobby mission endpoint

--- src/modules/lobby/repository/lobby.repository.ts
+++ src/modules/lobby/repository/lobby.repository.ts
@@ -24,6 +24,7 @@ export class LobbyRepository {
     return await this.prismaService.lobby.findUnique({
       where: { room_code: roomCode },
       include: {
+        players: true,
         missions: {
           include: {
             leader: {
